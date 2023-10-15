e3bddcc6a46150c9be5fcb00d068dcd6304a0676
cyrus phill <cyrus.fillmore@wsu.edu>
2023-10-14 23:03:36 -0700

    Added some example bios for the homepage.

--- client/src/home.svelte
+++ client/src/home.svelte
@@ -1,10 +1,30 @@
-<script>
-    let im = 'dubhack2023/CupOWater.jpg';
-</script> 
-
 <re1>
-    
-
+    Jeremy Johannson<br>
+    I like spicy food.<br>
+    1234,3564,2111<br>
 </re1>
+<re2>
+    Gordon Freeman<br>
+    I enjoy exotic food<br>
+    3141,1245,2122<br>
+</re2>
+<re3>
+    Hannibal Lectre<br>
+    I want something that pairs with a nice kiante<br>
+    3113,1392,1120
+</re3>
+<style>
+    re1{
+        color:rgb(213, 18, 155);
+        font-family: 'Arial, Helvetica, sans-serif';
+    }
+    re2{
+        color:crimson;
+        font-family: 'Arial, Helvetica, sans-serif';
+    }
+    re3{
+        color:darksalmon;
+        font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    }
 
-<img {im} alt = "Cup of Water"/>
+</style>
