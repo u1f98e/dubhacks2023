9683b6350b4458d6261ffc567f367de29dd2ac44
angelica.vargas <a.vargasescobar@outlook.com>
2023-10-14 22:50:06 -0700

    added boxes and descriptions to home screen lol

--- client/src/home.svelte
+++ client/src/home.svelte
@@ -1,49 +1,5 @@
-<<<<<<< HEAD
 <script>
 </script>
-=======
-
-
-
-<button on:click>
-<re1>
-    Jeremy Johannson<br>
-    I like spicy food.<br>
-    1234,3564,2111<br>
-    
-    
-</re1>
-</button>
-<br>
-<button>
-<re2>
-    Gordon Freeman<br>
-    I enjoy exotic food<br>
-    3141,1245,2122<br>
-</re2>
-</button>
-<br>
-<button>
-<re3>
-    Hannibal Lectre<br>
-    I want something that pairs with a nice kiante<br>
-    3113,1392,1120
-</re3>
-</button>
-<style>
-    re1{
-        color:rgb(213, 18, 155);
-        font-family: 'Arial, Helvetica, sans-serif';
-    }
-    re2{
-        color:crimson;
-        font-family: 'Arial, Helvetica, sans-serif';
-    }
-    re3{
-        color:darksalmon;
-        font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-    }
->>>>>>> 33bb477453fbcec2ec7d95d5d65063a61e5e6d46
 
 <div class="recipes-title">Profiles you'd like...</div>
 <div class = "container">
