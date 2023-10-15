909cbc4c12ac868a6c4a76b3691ae429c107b916
angelica.vargas <a.vargasescobar@outlook.com>
2023-10-14 20:46:07 -0700

    added title and fixed header container

--- client/src/navbar.svelte
+++ client/src/navbar.svelte
@@ -4,39 +4,15 @@
     import Home from "./home.svelte"
     import About from "./about.svelte"
     export let menu = 1;
-
 </script>
 
-<style>
-
-nav {
-        background-color: #333;
-        color: white;
-        height: 60px;
-    }
-
-a {
-    color: white;
-}
-
-ul li {
-    display : inline;
-    color: white;
-}
-
-</style>
-
- <main>
-    <nav>
-        <ul id="menu">
-            <li><a href="/" on:click|preventDefault={() => (menu = 1)}>Home</a></li>  
-            <li><a href="/" on:click|preventDefault={() => (menu = 2)}>About</a></li>
+<div class="header-container">
+    <div class="title">Recipe Maker</div>
+        <ul class="menu">
+                <li><a href="/" on:click|preventDefault={() => (menu = 1)}>Home</a></li>  
+                <li><a href="/" on:click|preventDefault={() => (menu = 2)}>About</a></li>
         </ul>
-        
-
-        
-    </nav>
- </main>
+</div>
 
  {#if menu === 1}
             <Home />
@@ -47,3 +23,34 @@ ul li {
 	            Page Not Found
             </h1>
 {/if}
+
+<style>
+    .header-container {
+        background-color: #333;
+        color: white;
+        height: 70px;
+    }
+
+    .title {
+        padding: 10px;
+        font-size: 40px;
+        font-family: 'Lora';
+        font-weight: 250;
+    }
+
+    .menu {
+        list-style: none;
+        display: flex;
+    }
+
+    .menu li {
+        display: inline;
+    }
+
+    .menu a {
+        text-decoration: none;
+        color: orange;
+        text-align: right;
+    }
+
+</style>
